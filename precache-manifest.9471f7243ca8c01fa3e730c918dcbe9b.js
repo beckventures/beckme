3d5cb5a2e9156ae16ea8226478598e4ad885a5e7
contactbeckfriends <contactbeckfriends@gmail.com>
2020-12-13 15:17:29 +0530

    Updates

---
+++ precache-manifest.9471f7243ca8c01fa3e730c918dcbe9b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec9baf2cad689a7e7e877d063f43968b",
+    "url": "/beckme/index.html"
+  },
+  {
+    "revision": "abfe00b502a23dc9d2e9",
+    "url": "/beckme/static/css/2.a77cddac.chunk.css"
+  },
+  {
+    "revision": "0cf4b0e06c98da90febc",
+    "url": "/beckme/static/css/main.edfa1764.chunk.css"
+  },
+  {
+    "revision": "abfe00b502a23dc9d2e9",
+    "url": "/beckme/static/js/2.12e0deb1.chunk.js"
+  },
+  {
+    "revision": "3e32fc12b8b19d7754177bd1a210cf3c",
+    "url": "/beckme/static/js/2.12e0deb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cf4b0e06c98da90febc",
+    "url": "/beckme/static/js/main.f382f50b.chunk.js"
+  },
+  {
+    "revision": "27a0843fb7b5dca9c26d",
+    "url": "/beckme/static/js/runtime-main.0272ab8b.js"
+  },
+  {
+    "revision": "4ca86dcf33180cec39455e3b5ddba3fb",
+    "url": "/beckme/static/media/GT-Pressura-Pro-Regular.4ca86dcf.woff2"
+  },
+  {
+    "revision": "23475fcadf7adf710dd4b72d11b0c41d",
+    "url": "/beckme/static/media/GT-Walsheim-Pro-Condensed-Black.23475fca.woff2"
+  },
+  {
+    "revision": "d3e9b49b94bc3e9715a2331decebf96a",
+    "url": "/beckme/static/media/GT-Walsheim-Pro-Condensed-Regular.d3e9b49b.woff2"
+  },
+  {
+    "revision": "9ded577f93b24331f4faadfc5f834ae6",
+    "url": "/beckme/static/media/SourceSansPro-Black.9ded577f.ttf"
+  },
+  {
+    "revision": "c1678b46f7dd3f50ceac94ed4e0ad01a",
+    "url": "/beckme/static/media/SourceSansPro-Regular.c1678b46.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/beckme/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/beckme/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/beckme/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/beckme/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/beckme/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/beckme/static/media/logo.5d5d9eef.svg"
+  }
+]);
